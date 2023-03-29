adb514c79a4997f859d4c6bbfb1fa838a9a4ac64
Twomey75 <125928251+Twomey75@users.noreply.github.com>
2023-03-28 21:40:48 -0400

    a

---
+++ src/Song.java
@@ -0,0 +1,18 @@
+import java.io.File;
+
+public class Song extends Media implements MediaFile{
+    File file;
+
+    public Song(String title, String description, String filePath, String mediaTopic)
+    {
+        super(title, description, mediaTopic);
+        this.file = new File(filePath);
+        htmlString = "<h2>" + title + "</h2>" + "<p>" + description + "</p>" +  "<video width='300' autoplay>" + "<source src='" + file.toURI() +
+        "' type='video/mp4' />" + "Your browser does not support the video element.</video><hr />";
+    }
+
+    public File getFileName() 
+    {
+        return file;
+    }
+}
