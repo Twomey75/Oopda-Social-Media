fbb11f722db8fd4eeeb2b7e9a6835397ddb347a6
Twomey75 <125928251+Twomey75@users.noreply.github.com>
2023-03-28 22:41:14 -0400

    a

--- src/SocialMediaGUI.java
+++ src/SocialMediaGUI.java
@@ -72,7 +72,7 @@ public class SocialMediaGUI extends Application {
 
 	Image bacon = new Image("bacon", "A slice", "assets\\bacon.jpg", Topic.Bacon);
 	Video toothpaste = new Video("Toothpaste", "Minty Fresh", "assets\\toothpaste.mp4", Topic.Toothpaste);
-	Song sugar = new Song("Sugar", "Add some sweetness to life", "assets\\sugar.wav", Topic.Sugar, "Someone Lmao")
+	Song sugar = new Song("Sugar", "Add some sweetness to life", "assets\\sugar.wav", Topic.Sugar, "Def Leppard");
 	
 	/**
 	 * the start method of the JavaFX GUI
@@ -220,7 +220,7 @@ public class SocialMediaGUI extends Application {
 
 							if(mostUsed == null || !mostUsed.equals(tokenizer)){
 								mostUsed = tokenizer;
-								mediaEngine.loadContent(prevMedia + toothpaste.getHtmlString());
+								mediaEngine.loadContent(prevMedia + sugar.getHtmlString());
 							}
 						}
 					});
