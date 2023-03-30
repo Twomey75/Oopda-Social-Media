a136175562fb36ddee6d27946d19528ca115c8ef
Twomey75 <125928251+Twomey75@users.noreply.github.com>
2023-03-29 22:35:17 -0400

    Update MediaCollection.java

--- src/MediaCollection.java
+++ src/MediaCollection.java
@@ -20,12 +20,12 @@ public class MediaCollection {
         mediaCollection.put(Topic.Pancakes, new Video("Pancakes", "Nice pancakes yum", "assets//Pancakes.mp4", Topic.Pancakes));
         mediaCollection.put(Topic.Toothpaste, new Video("ToothPaste", "1% of doctors dont reccommend toothpaste!", "assets//Toothpaste.mp4", Topic.Toothpaste));
         mediaCollection.put(Topic.Sugar, new Song("Sugar", "sugar song","assets//Sugar.wav", Topic.Sugar, "artist"));
-        mediaCollection.put(Topic.Barbies, new Image("Barbies", "Buy barbies now", "assets//barbies.png", Topic.Barbies));
+        mediaCollection.put(Topic.Barbies, new Image("Barbies", "Buy barbies now", "assets//barbie.png", Topic.Barbies));
         mediaCollection.put(Topic.Investments, new Event(2022, 12, 4, "https://www.google.com", "Real estate investments", "Learn how to invest in real estate", Topic.Investments));
         mediaCollection.put(Topic.Superman, new Event(2023, 4, 2, "https://www.google.com", "MEET SUPERMAN", "Meet the hero who saved metropolis", Topic.Superman));
         mediaCollection.put(Topic.Hairstyles, new Image("Hair Styles", "If you're insecure about your hair, change it!", "assets//Hairstyles.jpg", Topic.Hairstyles));
         mediaCollection.put(Topic.Pizza, new Event(2023, 5, 25, "https://www.google.com", "Andrew's Pizza Party", "Come getta da pizza", Topic.Pizza));
-        mediaCollection.put(Topic.Romance, new Song("Bad Romance", "Bad romance is a good song","assets//BadRommance.wav", Topic.Romance, "Lady GAGA"));
+        mediaCollection.put(Topic.Romance, new Song("Bad Romance", "Bad romance is a good song","assets//BadRomance.wav", Topic.Romance, "Lady GAGA"));
         mediaCollection.put(Topic.Travel, new Ad("https://www.google.com","Hotel Trivago", "Get hotel rooms in isreal for 5000 a night(what a steal!)", Topic.Travel));
         mediaCollection.put(Topic.Grapes, new Ad("https://www.google.com","Grapes", "They did surgery on a grape", Topic.Grapes));
     }
